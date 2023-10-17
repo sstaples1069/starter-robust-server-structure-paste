3f7a0ece33c91bbf31a590e20f3c19229132cac6
sstaples1069 <scottstarkstaples@gmail.com>
2023-10-16 17:10:11 -0700

    robust server operational and tests written

--- src/app.js
+++ src/app.js
@@ -2,6 +2,44 @@ const express = require("express");
 const app = express();
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
+const pastes = require("./data/pastes-data");
+
+app.use(express.json());
+
+app.use("/pastes/:pasteId", (req, res, next) => {
+  const { pasteId } = req.params;
+  const foundPaste = pastes.find(paste => paste.id === Number(pasteId));
+  if (foundPaste) {
+    res.json({ data: foundPaste });
+  } else {
+    next(`Paste id not found: ${pasteId}`)
+  }
+})
+
+app.get("/pastes", (req,res) => {
+  res.json({ data: pastes });
+});
+
+let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
+
+app.post("/pastes", (req, res, next) => {
+  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
+  if (text) {
+    const newPaste = {
+      id: ++lastPasteId, // Increment last ID, then assign as the current ID
+      name,
+      syntax,
+      exposure,
+      expiration,
+      text,
+      user_id,
+    };
+    pastes.push(newPaste);
+    res.status(201).json({ data: newPaste });
+  } else {
+    res.sendStatus(400);
+  }
+});
 
 // Not found handler
 app.use((request, response, next) => {
