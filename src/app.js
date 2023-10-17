6531ac3b51689ca60ecb40c5731014642f2c919e
sstaples1069 <scottstarkstaples@gmail.com>
2023-10-16 18:20:20 -0700

    updated error handling 4.2.5

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use("/pastes/:pasteId", (req, res, next) => {
   if (foundPaste) {
     res.json({ data: foundPaste });
   } else {
-    next(`Paste id not found: ${pasteId}`)
+    next({ status: 404, message: `Paste id not found: ${pasteId}` })
   }
 })
 
@@ -20,25 +20,33 @@ app.get("/pastes", (req,res) => {
   res.json({ data: pastes });
 });
 
+// New middleware function to validate the request body
+function bodyHasTextProperty(req, res, next) {
+  const { data: { text } = {} } = req.body;
+  if (text) {
+    return next(); // Call `next()` without an error message if the result exists
+  }
+  next({
+    status: 400,
+    message: "A 'text' property is required."
+  });
+}
+
 let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
 
-app.post("/pastes", (req, res, next) => {
+app.post("/pastes", bodyHasTextProperty, (req, res, next) => {
   const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
-  if (text) {
-    const newPaste = {
-      id: ++lastPasteId, // Increment last ID, then assign as the current ID
-      name,
-      syntax,
-      exposure,
-      expiration,
-      text,
-      user_id,
-    };
-    pastes.push(newPaste);
-    res.status(201).json({ data: newPaste });
-  } else {
-    res.sendStatus(400);
-  }
+  const newPaste = {
+    id: ++lastPasteId, // Increment last ID, then assign as the current ID
+    name,
+    syntax,
+    exposure,
+    expiration,
+    text,
+    user_id,
+  };
+  pastes.push(newPaste);
+  res.status(201).json({ data: newPaste });  
 });
 
 // Not found handler
@@ -47,9 +55,10 @@ app.use((request, response, next) => {
 });
 
 // Error handler
-app.use((error, request, response, next) => {
+app.use((error, req, res, next) => {
   console.error(error);
-  response.send(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  res.status(status).json({ error: message });
 });
 
 module.exports = app;
